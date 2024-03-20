36f0e64140cea20c7e7d1f231c15726b9e9a4fcf
nandhithaparas <nandhithaparasuraman@gmail.com>
2024-03-20 22:23:12 +0530

    Day 7 (Backend setup)

---
+++ BackEnd/giftcraft/src/main/java/com/example/giftcraft/GiftcraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.giftcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftcraftApplication.class, args);
+	}
+
+}
---
+++ BackEnd/giftcraft/src/test/java/com/example/giftcraft/GiftcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giftcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
